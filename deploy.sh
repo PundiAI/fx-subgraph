d050eb203de5fe40ee778f1e0df332ae79e5deb1
zakir <80246097+zakir-code@users.noreply.github.com>
2023-05-15 11:22:09 +0800

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,20 +7,18 @@ changeSubgraph=$(find . -name '*.json' -not -path "./node_modules/*" | grep '[_a
 for subgraph in ${changeSubgraph}; do
   subgraphName=$(dirname "${subgraph:2}")
   if [[ ! -d "${subgraphName}" || "${subgraphName}.json" != "$(basename "${subgraph}")" ]]; then
-    echo "invalid subgraph: ${subgraph}"
-    exit 1
+    echo "invalid subgraph: ${subgraph}" && exit 1
   fi
-  subgraphNumber=$(cat "${subgraph}" | jq -r '. | length')
-  if [ ${subgraphNumber} -gt 2 ]; then
-    echo "can only contain one testnet and mainnet subgraph: ${subgraph}"
-    exit 1
+  subgraphNumber=$(jq -r '. | length' "${subgraph}")
+  if [ "${subgraphNumber}" -gt 2 ]; then
+    echo "can only contain one testnet and mainnet subgraph: ${subgraph}" && exit 1
   fi
-  if [ ${subgraphNumber} -le 0 ]; then
+  if [ "${subgraphNumber}" -le 0 ]; then
     continue
   fi
-  for info in $(cat "${subgraph}" | jq -r '.[] | @base64'); do
+  for info in $(jq -r '.[] | @base64' "${subgraph}"); do
     _jq() {
-      echo ${info} | base64 --decode | jq -r ${1}
+      echo "${info}" | base64 --decode | jq -r "${1}"
     }
     if [ "${subgraphName}" != "$(_jq '.subgraph_name')" ]; then
       echo "invalid subgraph name: $(_jq '.subgraph_name')"
@@ -36,12 +34,10 @@ for subgraph in ${changeSubgraph}; do
     if [ "${network}" == "mainnet" ]; then
       graphNodeUrl="${GRAPH_NODE_URL}"
       ipfsUrl="${GRAPH_IPFS_URL}"
-      graphAuthKey="${GRAPH_AUTH_KEY}"
       graphNodePubUrl="https://graph-node.functionx.io"
     elif [ "${network}" == "testnet" ]; then
       graphNodeUrl="${TESTNET_GRAPH_NODE_URL}"
       ipfsUrl="${TESTNET_GRAPH_IPFS_URL}"
-      graphAuthKey="${TESTNET_GRAPH_AUTH_KEY}"
       graphNodePubUrl="https://testnet-graph-node.functionx.io"
     else
       echo "invalid network: $network"
@@ -49,16 +45,18 @@ for subgraph in ${changeSubgraph}; do
     fi
     githubRepo=$(_jq '.github_repo')
     if [ "${githubRepo:0:18}" != "https://github.com" ]; then
-        echo "invalid github repository URL: ${githubRepo}"
-        continue
+      echo "invalid github repository URL: ${githubRepo}"
+      continue
     fi
-    if [ ${githubRepo:0-4:4} == ".git" ]; then
+    if [ "${githubRepo:0-4:4}" == ".git" ]; then
       githubRepo="${githubRepo%????}"
     fi
     branch=$(_jq '.branch')
     curl --connect-timeout 3 -s https://raw.githubusercontent.com/"${githubRepo:19}"/"${branch}"/package.json | jq .
-    git clone -b "${branch}" "${githubRepo}" "${subgraphName}_${version}"
-    cd "${subgraphName}_${version}"
+    if [ ! -d "${subgraphName}_${version}/.git" ]; then
+      git clone -b "${branch}" "${githubRepo}" "${subgraphName}_${version}"
+    fi
+    cd "${subgraphName}_${version}" || exit 1
     echo "update ${subgraph}: form ${lastVersion} to ${version} in the ${network}"
     if [ -f "yarn.lock" ]; then
       yarn
@@ -68,12 +66,15 @@ for subgraph in ${changeSubgraph}; do
       npm run codegen
     fi
     npx graph build
-    if [[ ${lastVersion} == "v0.0.0" && ${version} == "v0.0.1" ]]; then
-        npx graph create --node "${graphNodeUrl}" ${subgraphName}
+    if [[ ${lastVersion} == "v0.0.0" ]]; then
+      echo "Creating subgraph ${subgraphName} on Graph node"
+      if ! npx graph create --node "${graphNodeUrl}" "${subgraphName}"; then
+        echo "Failed to create ${subgraphName}!" && cd ..
+        continue
+      fi
     fi
-    npx graph deploy --ipfs "${ipfsUrl}" --node "${graphNodeUrl}" "${subgraphName}" > /dev/null 2>&1
-    result=$?
-    if [ $result -eq 0 ] ; then
+    echo "Deploying subgraph ${subgraphName} to Graph node"
+    if npx graph deploy --ipfs "${ipfsUrl}" --node "${graphNodeUrl}" "${subgraphName}" >/dev/null 2>&1; then
       echo "Deploying to Graph node"
       echo "Deployed to $graphNodePubUrl/subgraphs/name/${subgraphName}/graphql"
       echo "Subgraph endpoints:"
@@ -82,6 +83,6 @@ for subgraph in ${changeSubgraph}; do
     else
       echo "Failed to deploy ${subgraphName}!"
     fi
-    cd ..
+    cd .. || exit 1
   done
 done
